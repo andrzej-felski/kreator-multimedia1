7df1c3e4fbdb89b5e0fa56e0c5542606bb36e0d5
andrzej-felski <andrzej.felski@tkchopin.pl>
2025-11-21 11:56:47 +0100

    Add files via upload

---
+++ Kreator ofert - Multimedia1/LISTA OFERT.js
@@ -0,0 +1,768 @@
+const packages = [
+
+// Internet światłowodowy - 2025/093
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Black Week 300Mb+ + HBO Max',
+    cena: 55.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Black Week 600Mb+ + HBO Max',
+    cena: 65.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 24,
+  },
+
+// Internet światłowodowy - 2025/077
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Studencki Raj 150Mb+',
+    cena: 45.00,
+    cenaAktywacji: 9.99,
+    dlugoscUmowy: 12,
+    promocja: { cena: 24.99, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Studencki Raj 300Mb+',
+    cena: 55.00,
+    cenaAktywacji: 9.99,
+    dlugoscUmowy: 12,
+    promocja: { cena: 29.99, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Studencki Raj 600Mb+',
+    cena: 65.00,
+    cenaAktywacji: 9.99,
+    dlugoscUmowy: 12,
+    promocja: { cena: 34.99, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Studencki Raj 900Mb+',
+    cena: 75.00,
+    cenaAktywacji: 9.99,
+    dlugoscUmowy: 12,
+    promocja: { cena: 39.99, czasTrwania: 3 }
+  },
+
+// Internet światłowodowy - 2025/029
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 150Mb+',
+    cena: 45.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 10,
+    promocja: { cena: 1.23, czasTrwania: 1 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 300Mb+',
+    cena: 55.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 10,
+    promocja: { cena: 1.23, czasTrwania: 1 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 600Mb+',
+    cena: 65.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 10,
+    promocja: { cena: 1.23, czasTrwania: 1 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 900Mb+',
+    cena: 75.00,
+    cenaAktywacji: 29.99,
+    dlugoscUmowy: 10,
+    promocja: { cena: 1.23, czasTrwania: 1 }
+  },
+
+// Internet światłowodowy - 2025/030
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 150Mb+',
+    cena: 45.00,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 20,
+    promocja: { cena: 1.23, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 300Mb+',
+    cena: 55.00,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 20,
+    promocja: { cena: 1.23, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 600Mb+',
+    cena: 65.00,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 20,
+    promocja: { cena: 1.23, czasTrwania: 3 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Strzał w Dziesiątkę 900Mb+',
+    cena: 75.00,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 20,
+    promocja: { cena: 1.23, czasTrwania: 3 }
+  },
+
+// Internet światłowodowy - 2024/066
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 300Mb+',
+    cena: 73.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 600Mb+',
+    cena: 83.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 900Mb+',
+    cena: 88.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+
+// Internet światłowodowy - 2024/063
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 1,2Gb+',
+    cena: 118.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+
+// Internet światłowodowy - 2024/129
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 300Mb+',
+    cena: 68.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+    promocja: { cena: 61.45, czasTrwania: 7 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 600Mb+',
+    cena: 78.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+    promocja: { cena: 71.45, czasTrwania: 7 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 900Mb+',
+    cena: 83.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+    promocja: { cena: 76.45, czasTrwania: 7 }
+  },
+
+// Internet światłowodowy - 2024/130
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 1,2Gb+',
+    cena: 107.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+    promocja: { cena: 100.45, czasTrwania: 7 }
+  },
+
+// Internet światłowodowy - 2024/128
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 300Mb+',
+    cena: 73.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 999,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 600Mb+',
+    cena: 83.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 999,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 900Mb+',
+    cena: 88.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 999,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+
+// Internet światłowodowy - 2024/065
+  {
+    typ: 'Internet światłowodowy',
+    nazwa: 'Internet BIS 1,2Gb+',
+    cena: 118.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 999,
+  },
+
+// Internet mobilny - 2021/269
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 20 GB LTE',
+    cena: 19.99,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 100 GB LTE EXTRA',
+    cena: 44.99,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 200 GB LTE EXTRA',
+    cena: 64.99,
+    cenaAktywacji: 19.99,
+    dlugoscUmowy: 24,
+  },
+
+// Internet mobilny - 2021/268
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 20 GB LTE',
+    cena: 19.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 100 GB LTE EXTRA',
+    cena: 44.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Internet mobilny',
+    nazwa: 'Internet Mobilny 200 GB LTE EXTRA',
+    cena: 64.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+
+// Telewizja - 2024/122
+  {
+    typ: 'Telewizja',
+    nazwa: 'StartBox - 66 kanałów TV',
+    cena: 42.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Telewizja',
+    nazwa: 'SuperBox - 139 kanałów TV',
+    cena: 62.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Telewizja',
+    nazwa: 'MaxBox - 159 kanałów TV',
+    cena: 72.99,
+    cenaAktywacji: 98.99,
+    dlugoscUmowy: 12,
+  },
+
+// Telewizja - 2024/121
+  {
+    typ: 'Telewizja',
+    nazwa: 'StartBox - 66 kanałów TV',
+    cena: 35.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Telewizja',
+    nazwa: 'SuperBox - 139 kanałów TV',
+    cena: 55.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Telewizja',
+    nazwa: 'MaxBox - 159 kanałów TV',
+    cena: 65.99,
+    cenaAktywacji: 78.99,
+    dlugoscUmowy: 24,
+  },
+
+// Premium TV - 2022/104
+  {
+    typ: 'Premium TV',
+    nazwa: 'Dzieci',
+    cena: 6.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Filmy i Seriale',
+    cena: 7.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'HD',
+    cena: 9.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Filmbox Extra',
+    cena: 10.00,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Sport HD Extra',
+    cena: 10.00,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Cinemax',
+    cena: 10.00,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Fun Club',
+    cena: 14.99,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 12,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Eleven Sports',
+    cena: 14.99,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 12,
+  },
+
+// Premium TV - 2022/103
+  {
+    typ: 'Premium TV',
+    nazwa: 'Dzieci',
+    cena: 6.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Filmy i Seriale',
+    cena: 7.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'HD',
+    cena: 9.99,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Filmbox Extra',
+    cena: 10.00,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Sport HD Extra',
+    cena: 10.00,
+    cenaAktywacji: 1.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Cinemax',
+    cena: 10.00,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Fun Club',
+    cena: 14.99,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Premium TV',
+    nazwa: 'Eleven Sports',
+    cena: 14.99,
+    cenaAktywacji: 4.99,
+    dlugoscUmowy: 24,
+  },
+
+// Premium TV - 2024/134
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Super Sport',
+    cena: 65.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 12,
+  },
+
+// Premium TV - 2024/133
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Super Sport',
+    cena: 65.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 24,
+  },
+
+// Premium TV - 2024/133
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Super Sport',
+    cena: 69.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 999,
+  },
+
+// Premium TV - 2024/134
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Seriale i Filmy',
+    cena: 25.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 12,
+  },
+
+// Premium TV - 2024/133
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Seriale i Filmy',
+    cena: 25.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 24,
+  },
+
+// Premium TV - 2024/133
+  {
+    typ: 'Premium TV',
+    nazwa: 'Canal+ Seriale i Filmy',
+    cena: 29.00,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 999,
+  },
+
+// Premium TV - 2025/065
+  {
+    typ: 'Premium TV',
+    nazwa: 'HBO',
+    cena: 29.99,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 12,
+  },
+
+// Premium TV - 2025/067
+  {
+    typ: 'Premium TV',
+    nazwa: 'HBO',
+    cena: 29.99,
+    cenaAktywacji: 10.00,
+    dlugoscUmowy: 24,
+  },
+
+// Internet i Telewizja - 2024/124
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 300Mb+',
+    cena: 48.99,
+    cenaAktywacji: 40.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 41.45, czasTrwania: 7 }
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 600Mb+',
+    cena: 58.99,
+    cenaAktywacji: 40.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 51.45, czasTrwania: 7 }
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 900Mb+',
+    cena: 68.99,
+    cenaAktywacji: 40.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 61.45, czasTrwania: 7 }
+  },
+
+// Internet i Telewizja - 2024/125
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 1,2Gb+',
+    cena: 73.99,
+    cenaAktywacji: 40.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 66.45, czasTrwania: 7 }
+  },
+
+// Internet i Telewizja - 2024/123
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'StartBox - 66 kanałów TV',
+    cena: 36.99,
+    cenaAktywacji: 39.99,
+    dlugoscUmowy: 24,
+  },
+
+// Internet i Telewizja - 2024/124
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'SuperBox - 139 kanałów TV',
+    cena: 51.99,
+    cenaAktywacji: 39.99,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'MaxBox - 159 kanałów TV',
+    cena: 61.99,
+    cenaAktywacji: 39.99,
+    dlugoscUmowy: 24,
+  },
+
+// Internet i Telewizja - 2024/126
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 300Mb+',
+    cena: 48.99,
+    cenaAktywacji: 49.00,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 600Mb+',
+    cena: 58.99,
+    cenaAktywacji: 49.00,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 900Mb+',
+    cena: 68.99,
+    cenaAktywacji: 49.00,
+    dlugoscUmowy: 999,
+  },
+
+// Internet i Telewizja - 2024/127
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'Internet BIS 1,2Gb+',
+    cena: 73.99,
+    cenaAktywacji: 49.00,
+    dlugoscUmowy: 999,
+  },
+
+// Internet i Telewizja - 2024/126
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'SuperBox - 139 kanałów TV',
+    cena: 56.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Internet i Telewizja',
+    nazwa: 'MaxBox - 159 kanałów TV',
+    cena: 66.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+
+// Abonament komórkowy - 2022/002
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 10 GB',
+    cena: 19.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 30 GB',
+    cena: 29.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 60 GB',
+    cena: 39.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+  },
+
+// Abonament komórkowy - 2022/004
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 10 GB',
+    cena: 19.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 1.00, czasTrwania: 4 }
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 30 GB',
+    cena: 29.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 1.00, czasTrwania: 4 }
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 60 GB',
+    cena: 39.99,
+    cenaAktywacji: 0.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 1.00, czasTrwania: 4 }
+  },
+
+// Abonament komórkowy - 2022/002
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 10 GB',
+    cena: 19.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 30 GB',
+    cena: 29.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+  {
+    typ: 'Abonament komórkowy',
+    nazwa: 'multiMobilny + 60 GB',
+    cena: 39.99,
+    cenaAktywacji: 49.99,
+    dlugoscUmowy: 999,
+  },
+
+// Telefon stacjonarny - 2021/256
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Standardowy 50',
+    cena: 19.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 12,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Standardowy 200',
+    cena: 29.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 12,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Rodzinny 400',
+    cena: 39.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 12,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Bez limitu',
+    cena: 39.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 12,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Bez limitu - Świat',
+    cena: 49.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 12,
+    promocja: { cena: 0.00, czasTrwania: 1 }
+  },
+
+// Telefon stacjonarny - 2021/257
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Standardowy 50',
+    cena: 19.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 0.00, czasTrwania: 3 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Standardowy 200',
+    cena: 29.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 0.00, czasTrwania: 3 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Rodzinny 400',
+    cena: 39.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 0.00, czasTrwania: 3 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Bez limitu',
+    cena: 39.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 0.00, czasTrwania: 3 }
+  },
+  {
+    typ: 'Telefon stacjonarny',
+    nazwa: 'Bez limitu - Świat',
+    cena: 49.99,
+    cenaAktywacji: 19.00,
+    dlugoscUmowy: 24,
+    promocja: { cena: 0.00, czasTrwania: 3 }
+  },
+];
